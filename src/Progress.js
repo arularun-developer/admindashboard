64ac350c12c6523e27b78a7ce18097642e976d3e
arularun-developer <arularunoffical1110@gmail.com>
2023-06-23 15:26:29 +0530

    changed

--- src/Progress.js
+++ src/Progress.js
@@ -6,7 +6,11 @@ function Progress(props) {
       <div className="card-body">
               <h4 className="small font-weight-bold">{props.data.name} <span className="float-right">{props.data.value}%</span></h4>
               <div className="progress mb-4">
-                <div className={`progress-bar ${props.data.color}`} role="progressbar" style={{width: `${props.data.value}%`}} aria-valuenow={`${props.data.value}`} aria-valuemin="0" aria-valuemax="100" />
+                <div className={`progress-bar ${props.data.color}`}
+                 role="progressbar" 
+                 style={{width: `${props.data.value}%`}}
+                 
+                  />
               </div>
             </div>
           
